78bc0dfad51c6b7b137467faeaf9bf55fe827631
KevKibe <kchegz234@gmail.com>
2023-06-29 22:13:57 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
+import sys
 from scraping import ArticleFetcher
 from query_utils import ConversationChain
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv('.env')
 
 urls = [
     'https://medium.com/@TechTalkWithAlex/creating-your-first-ever-recommendation-system-like-netflix-5a30e7c461a3',
@@ -16,7 +17,6 @@ fetcher.fetch_articles()
 
 articles = fetcher.get_articles()
 
-print(articles)
 openai_api_key = os.getenv('OPENAI_API_KEY')
 conversation = ConversationChain(openai_api_key)
 preprocessed_articles = []
@@ -24,8 +24,30 @@ for article in articles:
     preprocessed_text = conversation.preprocess_text(article['content'])  
     preprocessed_articles.append(preprocessed_text)
 
-print(preprocessed_articles)
-text_chunks = conversation.get_text_chunks(preprocessed_text)
+text_chunks = []
+for preprocessed_text in preprocessed_articles:
+    chunks = conversation.get_text_chunks(preprocessed_text)
+    text_chunks.extend(chunks)
+
 vectorstore = conversation.get_vectorstore(text_chunks)
 conversation_chain = conversation.get_conversation_chain(vectorstore)
 
+yellow = "\033[0;33m"
+green = "\033[0;32m"
+white = "\033[0;39m"
+
+chat_history = []
+print(f"{yellow}---------------------------------------------------------------------------------")
+print('Welcome to the DocBot. You are now ready to start interacting with your documents')
+print('---------------------------------------------------------------------------------')
+while True:
+    query = input(f"{green}Prompt: ")
+    if query == "exit" or query == "quit" or query == "q" or query == "f":
+        print('Exiting')
+        sys.exit()
+    if query == '':
+        continue
+    result = conversation_chain(
+        {"question": query, "chat_history": chat_history})
+    print(f"{white}Answer: " + result["answer"])
+    chat_history.append((query, result["answer"]))
--- query_utils.py
+++ query_utils.py
@@ -1,9 +1,7 @@
-import nltk
-import re
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import FAISS
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.memory import ConversationBufferMemory
@@ -27,13 +25,14 @@ class ConversationChain:
             chunk_overlap=200,
             length_function=len
         )
-        chunks = text_splitter.split_documents(text)
-        return chunks
+        text_chunks = text_splitter.split_text(text)
+        return text_chunks
 
-    def get_vectorstore(self, text_chunks):
-        vectordb = Chroma.from_documents(text_chunks, embedding=OpenAIEmbeddings(openai_api_key=self.openai_api_key))
-        return vectordb
-
+    def get_vectorstore(text_chunks):
+        embeddings = OpenAIEmbeddings()
+        vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+        return vectorstore
+    
     def get_conversation_chain(self, vectorstore):
         llm = ChatOpenAI()
 
