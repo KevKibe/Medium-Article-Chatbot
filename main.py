26262d36cbb3191568e763844b820e2253685af3
KevKibe <kchegz234@gmail.com>
2023-06-30 21:11:52 +0300

    update

--- main.py
+++ main.py
@@ -5,67 +5,62 @@ from query_utils import ConversationChain
 from dotenv import load_dotenv
 load_dotenv('.env')
 
-class ArticleChatBot:
-    def __init__(self, urls, openai_api_key):
-        self.fetcher = ArticleFetcher(urls)
-        self.openai_api_key = openai_api_key
-        self.conversation = None
-        self.chat_history = []
-
-    def fetch_articles(self):
-        self.fetcher.fetch_articles()
-
-    def preprocess_articles(self):
-        articles = self.fetcher.get_articles()
+class MediumArticleChatbot:
+    def __init__(self, urls):
+        self.urls = urls
+        self.openai_api_key = os.getenv('OPENAI_API_KEY')
         self.conversation = ConversationChain(self.openai_api_key)
-        self.preprocessed_articles = [
-            self.conversation.preprocess_text(article['content'])
-            for article in articles
-        ]
+        self.chat_history = []
+        self.vectorstore = None
+        self.conversation_chain = None
 
-    def generate_text_chunks(self):
-        self.text_chunks = [
-            chunk
-            for preprocessed_text in self.preprocessed_articles
-            for chunk in self.conversation.get_text_chunks(preprocessed_text)
-        ]
+    def fetch_and_preprocess_articles(self):
+        fetcher = ArticleFetcher(self.urls)
+        fetcher.fetch_articles()
+        articles = fetcher.get_articles()
+        preprocessed_articles = []
+        for article in articles:
+            preprocessed_text = self.conversation.preprocess_text(article['content'])
+            preprocessed_articles.append(preprocessed_text)
+        return preprocessed_articles
 
-    def setup_chatbot(self):
-        self.fetch_articles()
-        self.preprocess_articles()
-        self.generate_text_chunks()
+    def get_text_chunks(self, preprocessed_articles):
+        text_chunks = []
+        for preprocessed_text in preprocessed_articles:
+            chunks = self.conversation.get_text_chunks(preprocessed_text)
+            text_chunks.extend(chunks)
+        return text_chunks
 
-    def run_chatbot(self):
+    def setup(self):
+        load_dotenv('.env')
+        preprocessed_articles = self.fetch_and_preprocess_articles()
+        text_chunks = self.get_text_chunks(preprocessed_articles)
+        self.vectorstore = self.conversation.get_vectorstore(text_chunks)
+        self.conversation_chain = self.conversation.get_conversation_chain(self.vectorstore)
+
+    def run(self):
         yellow = "\033[0;33m"
         green = "\033[0;32m"
         white = "\033[0;39m"
-
         print(f"{yellow}---------------------------------------------------------------------------------")
         print('Start your chat-based interaction with your articles')
         print('---------------------------------------------------------------------------------')
         while True:
             query = input(f"{green}Prompt: ")
-            if query in ["exit", "quit", "q", "f"]:
-                print('\033[32m'+'Exiting')
+            if query == "exit" or query == "quit" or query == "q" or query == "f":
+                print('\033[32m' + 'Exiting')
                 sys.exit()
-            if not query:
+            if query == '':
                 continue
-            result = self.conversation.get_conversation_chain(self.text_chunks, self.chat_history, {"question": query})
+            result = self.conversation_chain({"question": query, "chat_history": self.chat_history})
             print(f"{white}Answer: " + result["answer"])
             self.chat_history.append((query, result["answer"]))
 
-# URLs of the articles to fetch
-urls = [
-    'https://medium.com/@TechTalkWithAlex/creating-your-first-ever-recommendation-system-like-netflix-5a30e7c461a3',
-    'https://medium.com/gitconnected/different-approaches-for-building-recommender-systems-using-python-1bd2179fdc8a',
-]
-
-# OpenAI API key
-openai_api_key = os.getenv('OPENAI_API_KEY')
-
-# Create an instance of the ArticleChatBot
-chatbot = ArticleChatBot(urls, openai_api_key)
-
-# Setup and run the chatbot
-chatbot.setup_chatbot()
-chatbot.run_chatbot()
+if __name__ == '__main__':
+    urls = [
+        'https://medium.com/@TechTalkWithAlex/creating-your-first-ever-recommendation-system-like-netflix-5a30e7c461a3',
+        'https://medium.com/gitconnected/different-approaches-for-building-recommender-systems-using-python-1bd2179fdc8a',
+    ]
+    chatbot = MediumArticleChatbot(urls)
+    chatbot.setup()
+    chatbot.run()
--- test.py
+++
@@ -1,4 +0,0 @@
-import openai
-openai.api_key = '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'
-
-print(openai.Model.list())
