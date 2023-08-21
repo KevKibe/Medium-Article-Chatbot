4ca2da95cae157d094a6614d8634105d179f92bd
KevKibe <kchegz234@gmail.com>
2023-08-21 14:36:58 +0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,8 @@ import requests
 def main():
     st.title("Medium Article Chatbot")
 
-    url = st.text_input("User: Enter the URL of the Medium article:")
-    query = st.text_input("User: Enter your query:")
+    url = st.text_input("Enter the URL of the Medium article:")
+    query = st.text_input("Enter your query:")
 
     if url and query:
         payload = {"url": url, "query": query}
@@ -14,22 +14,9 @@ def main():
         if response.status_code == 200:
             response_data = response.json()
             answer = response_data.get("answer")
-            
-            st.text("Chatbot:")
-            st.info(answer)
-            
-    new_query = st.text_input("User: Enter another query:")
-
-    if new_query:
-        payload = {"url": url, "query": new_query}
-        response = requests.post("http://54.216.44.96:8080/chat", json=payload)
-
-        if response.status_code == 200:
-            response_data = response.json()
-            answer = response_data.get("answer")
-            
-            st.text("Chatbot:")
-            st.info(answer)
+            st.write("Response:", answer)
+        else:
+            st.write("Error occurred while retrieving response from the API")
 
 if __name__ == "__main__":
     main()
