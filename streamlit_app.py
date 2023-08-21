366162d74f4830fca33b60e18364355b3f535fc3
KevKibe <kchegz234@gmail.com>
2023-08-21 14:13:05 +0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,20 +7,29 @@ def main():
     url = st.text_input("User: Enter the URL of the Medium article:")
     query = st.text_input("User: Enter your query:")
 
-    while query:
-        if url and query:
-            payload = {"url": url, "query": query}
-            response = requests.post("http://54.216.44.96:8080/chat", json=payload)
+    if url and query:
+        payload = {"url": url, "query": query}
+        response = requests.post("http://54.216.44.96:8080/chat", json=payload)
 
-            if response.status_code == 200:
-                response_data = response.json()
-                answer = response_data.get("answer")
-                
-                st.text("Chatbot:")
-                st.info(answer)
+        if response.status_code == 200:
+            response_data = response.json()
+            answer = response_data.get("answer")
+            
+            st.text("Chatbot:")
+            st.info(answer)
+            
+    new_query = st.text_input("User: Enter another query:")
 
-        new_query = st.text_input("User: Enter another query (or leave empty to end):")
-        query = new_query
+    if new_query:
+        payload = {"url": url, "query": new_query}
+        response = requests.post("http://54.216.44.96:8080/chat", json=payload)
+
+        if response.status_code == 200:
+            response_data = response.json()
+            answer = response_data.get("answer")
+            
+            st.text("Chatbot:")
+            st.info(answer)
 
 if __name__ == "__main__":
     main()
