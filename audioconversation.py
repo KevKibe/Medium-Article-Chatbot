1253e4f2b510cb4550aae2344823f9bf20a5a63d
KevKibe <kchegz234@gmail.com>
2023-08-28 14:14:48 +0300

    update

--- audioconversation.py
+++ audioconversation.py
@@ -1,26 +1,27 @@
 from transcription import SpeechRecognition
 from audio_gen import TextToSpeech
+from main import MediumArticleChatbot
 import requests
 import time
 
 speech_recognition = SpeechRecognition()
 speech_generation = TextToSpeech()
 
+
 class Conversation:
     def __init__(self, url):
-        self.url = url
+        self.urls = url
 
     def record_and_transcribe_audio(self):
         audio_data = speech_recognition.record_audio()
         query = speech_recognition.transcribe_audio(audio_data)
         return query
 
-    def send_query_to_server(self, query):
-        payload = {"url": self.url, "query": query}
-        response = requests.post("http://3.253.153.46:8080/chat", json=payload)
-        response_data = response.json()
-        answer = response_data.get("answer")
-        return answer
+    def get_response(self, query):
+        chatbot = MediumArticleChatbot(self.urls)
+        chatbot.setup()
+        response = chatbot.generate_response(query)
+        return response
 
     def run(self):
         while True:
@@ -28,7 +29,7 @@ class Conversation:
             print("User query:", query)
 
             start_time = time.time()
-            answer = self.send_query_to_server(query)
+            answer = self.get_response(query)
             print("Response:", answer)
 
             end_time = time.time()
@@ -38,7 +39,8 @@ class Conversation:
             elapsed_time = end_time - start_time
             print(f"Time: {elapsed_time:.2f} seconds\n")
 
-url = "https://thebolditalic.com/why-im-breaking-up-with-burning-man-efe43bb5c9c5"
+url = ["https://medium.com/backenders-club/api-design-best-practices-a-deep-dive-2022-ec5a19dc27cc",
+       "https://medium.com/javarevisited/10-rest-api-best-practices-cd12e3904d00"]
 
 convo = Conversation(url)
 convo.run()
